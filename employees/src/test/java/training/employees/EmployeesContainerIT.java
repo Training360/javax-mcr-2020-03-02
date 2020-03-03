cf82aecd22ede67cadebb98a2b557f0cc899eb78
vicziani <viczian.istvan@gmail.com>
2020-03-03 14:20:34 +0100

    Web container integration test

--- employees/src/test/java/training/employees/EmployeesContainerIT.java
+++ employees/src/test/java/training/employees/EmployeesContainerIT.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class EmployeesContainerIT {
@@ -17,11 +21,16 @@ public class EmployeesContainerIT {
     @Test
     void testCreateEmployee() {
         template.postForObject("/api/employees",
-                new CreateEmployeeCommand("John Doe"),
+                new CreateEmployeeCommand("John Smith"),
                 EmployeeDto.class);
 
         List<EmployeeDto> employees =
-                template.getForObject("/api/employees", List.class);
-        System.out.println(employees);
+                template.exchange("/api/employees", HttpMethod.GET,
+                        null,
+                        new ParameterizedTypeReference<List<EmployeeDto>>() {}).getBody();
+
+        assertThat(employees)
+                .extracting(EmployeeDto::getName)
+                .containsExactly("John Doe", "Jane Doe", "John Smith");
     }
 }
