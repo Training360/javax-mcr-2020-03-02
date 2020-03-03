a765d88f11f005e38916df689874758f6a881c98
vicziani <viczian.istvan@gmail.com>
2020-03-03 16:32:24 +0100

    JDBC

---
+++ employees/src/main/java/training/employees/EmployeesRepository.java
@@ -0,0 +1,35 @@
+package training.employees;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+@Repository
+public class EmployeesRepository {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public EmployeesRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Employee saveEmployee(Employee employee) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(
+                con -> {
+                    PreparedStatement ps =
+                            con.prepareStatement("insert into employees(emp_name) values (?)",
+                                    Statement.RETURN_GENERATED_KEYS);
+                    ps.setString(1, employee.getName());
+                    return ps;
+                }, keyHolder);
+
+        employee.setId(keyHolder.getKey().longValue());
+        return employee;
+    }
+}
