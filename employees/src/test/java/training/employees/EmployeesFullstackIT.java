e0f1341492571e9601b245b5f9c1221495f5d555
vicziani <viczian.istvan@gmail.com>
2020-03-04 11:42:16 +0100

    SQL

--- employees/src/test/java/training/employees/EmployeesFullstackIT.java
+++ employees/src/test/java/training/employees/EmployeesFullstackIT.java
@@ -3,10 +3,12 @@ package training.employees;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@Sql(statements = "delete from employees")
 public class EmployeesFullstackIT {
 
     @Autowired
